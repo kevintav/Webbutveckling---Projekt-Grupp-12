381c1f5795643d8f24e9334233b82a3aa6940519
kevintav <kevin@amanzi.com>
2026-01-04 16:54:35 +0100

    Made changes to furthur seperate concern, moved REGION_MAP to jobs_service.py

--- backend/api/search.py
+++ backend/api/search.py
@@ -9,18 +9,6 @@ def search(
         municipality: str = Query(..., description="Municipality")
 ):
 
-    REGION_MAP = {
-        "Skåne": "CaRE_1nn_cSU",
-        "Stockholm": "oDpK_oZ2_WYt",
-        "Västra Götalands": "zdoY_6u5_Krt",
-        "Uppsala": "zBon_eET_fFU",
-        "Norrbotten": "9hXe_F4g_eTG",
-        "Jönköpings": "MtbE_xWT_eMi",
-        "Västmanlands": "G6DV_fKE_Viz",
-    }
-
-    region_id = REGION_MAP.get(municipality)
-
-    return fetch_jobs(q, region_id)
+    return fetch_jobs(q, municipality)
 
 
--- backend/services/jobs_service.py
+++ backend/services/jobs_service.py
@@ -2,10 +2,20 @@ import requests
 
 JOBSEARCH_API_URL = "https://jobsearch.api.jobtechdev.se/search"
 
+REGION_MAP = {
+        "Skåne": "CaRE_1nn_cSU",
+        "Stockholm": "oDpK_oZ2_WYt",
+        "Västra Götalands": "zdoY_6u5_Krt",
+        "Uppsala": "zBon_eET_fFU",
+        "Norrbotten": "9hXe_F4g_eTG",
+        "Jönköpings": "MtbE_xWT_eMi",
+        "Västmanlands": "G6DV_fKE_Viz",
+    }
+
 def fetch_jobs(query: str, municipality: str):
     params = {
         "q": query,
-        "region": municipality,
+        "region": REGION_MAP.get(municipality),
         "limit": 10
     }
 
